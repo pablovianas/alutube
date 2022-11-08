da054e658154381bad3aa6850a5e48266aa9085e
pablovianas <pabloviana2010@gmail.com>
2022-11-08 16:01:19 -0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import { StyledHeader } from "./Header..style"
+import config from '../../../config.json'
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
+            </section>
+        </StyledHeader>
+    )
+}
---
+++ src/components/Timeline/Timeline.js
@@ -0,0 +1,31 @@
+import { StyledTimeline } from "./Timeline.style"
+
+export const Timeline = (props) => {
+    // console.log("Dentro do componente", props.playlists);
+    const playlistNames = Object.keys(props.playlists);
+
+    return (
+        <StyledTimeline>
+            {playlistNames.map((playlistName) => {
+                const videos = props.playlists[playlistName];
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos.map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img src={video.thumb} />
+                                        <span>
+                                            {video.title}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
