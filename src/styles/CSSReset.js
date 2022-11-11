5f9aad39800389ff18ea48ed870ee419b097eb16
pablovianas <pabloviana2010@gmail.com>
2022-11-11 11:55:53 -0300

    Added custom scrollbar

--- src/styles/CSSReset.js
+++ src/styles/CSSReset.js
@@ -30,4 +30,25 @@ export const CSSReset = createGlobalStyle`
         opacity: .5;
         }
     }
+
+    ::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+    
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #888; 
+        border-radius: 10px ;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555; 
+    }
 `;
